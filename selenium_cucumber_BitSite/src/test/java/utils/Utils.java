09d37b1e19c11ce854e1486b4080edba7e6ac4ee
Valeriu Chirica <ValeriuChirica@Valerius-MacBook-Pro.local>
2020-01-16 21:41:15 -0500

    some shit

---
+++ selenium_cucumber_BitSite/src/test/java/utils/Utils.java
@@ -0,0 +1,29 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Utils {
+	
+	
+
+	public static List<String> stringlist(List<WebElement> list){
+
+		
+		SingletonBrowser sb = SingletonBrowser.getSingletonBrowser();
+		WebDriver driver = sb.getDriver();
+		
+		
+		list = driver.findElements(By.cssSelector("[class='main-navigation'] li"));
+		List<String> list1 = new ArrayList<String>();
+
+		list.forEach(s -> list1.add(s.getText()));
+
+		return list1;
+
+	}
+} 
