a7ea03898a958d541d0e98d83f48aa88d5b5fda3
Igor Shendarovich <ishendarovich@gmail.com>
2020-01-18 10:01:21 -0500

    Igor Changes Home page

--- selenium_cucumber_BitSite/src/test/java/pages/HomePageTest.java
+++ selenium_cucumber_BitSite/src/test/java/pages/HomePageTest.java
@@ -31,9 +31,42 @@ public class HomePageTest {
 	@FindBy(css = "[class='main-navigation'] li a")
 	public List<WebElement> menubarName;
 	
+	@FindBy(css = "[data-id='e5e7443'] [class ='elementor-heading-title elementor-size-default']")
+	public WebElement ourServices;
+	
+	@FindBy(css = "[data-id='874b8fb'] [class = 'elementor-button-content-wrapper']")
+	public WebElement qaTestingLM;
+	
+	@FindBy(css = "[data-id='7ef62b9'] [class = 'elementor-button-text']")
+	public WebElement softwareTestingLM;
+	
+	@FindBy(css = "[data-id='88120ac'] [class = 'elementor-button-content-wrapper']\r\n" + 
+			"")
+	public WebElement onsiteTrainingsLM;
+	
+		
 	@FindBy(css ="[data-id='6d0d9cb'] [class ='elementor-button-wrapper'] a")
 	public WebElement FirstRegButton;
 	
+	@FindBy(css ="[data-id='4dc4ffc'] [class = 'elementor-button-text']\r\n" + 
+			"")
+	public WebElement SecondRegButton;
+	
+	
+	@FindBy(css ="[id = 'menu-item-13']")
+	public WebElement homeButton;
+	
+	@FindBy(css ="[id ='menu-item-12']")
+	public WebElement aboutButton;
+	
+	@FindBy(css ="[id ='menu-item-289']")
+	public WebElement servicesButton;
+	
+	@FindBy(css ="[id = 'menu-item-288']")
+	public WebElement bootCampButton;
+	
+	@FindBy(css ="[id = 'menu-item-150']")
+	public WebElement contactButton;
 
 	
 	
