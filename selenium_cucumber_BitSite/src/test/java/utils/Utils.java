a463f181663eebb86662a5f4249315226fe52d1a
valeriu02 <57537134+valeriu02@users.noreply.github.com>
2020-01-22 11:37:32 -0500

    Update Utils.java

--- selenium_cucumber_BitSite/src/test/java/utils/Utils.java
+++ selenium_cucumber_BitSite/src/test/java/utils/Utils.java
@@ -18,29 +18,6 @@ public class Utils {
 		WebDriver driver = sb.getDriver();
 		
 		
-		
-		List<String> list1 = new ArrayList<String>();
-
-		list.forEach(s -> list1.add(s.getText()));package utils;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-public class Utils {
-	
-	
-
-	public static List<String> stringlist(List<WebElement> list){
-
-		
-		SingletonBrowser sb = SingletonBrowser.getSingletonBrowser();
-		WebDriver driver = sb.getDriver();
-		
-		
 		list = driver.findElements(By.cssSelector("[class='elementor-widget-container'] h2"));
 		List<String> list1 = new ArrayList<String>();
 		list.forEach(s -> list1.add(s.getText()));
@@ -52,14 +29,3 @@ public class Utils {
 	
 
 	}
-
-
-
-
-
-
-
-		return list1;
-
-	}
-} 
